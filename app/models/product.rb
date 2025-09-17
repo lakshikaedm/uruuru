854cbea90a591ba9ef49fe2442b238833cb032f1
Lakshika Sandaruwan <lakshikaedm@gmail.com>
2025-09-17 22:31:39 +0900

    style(products): add Tailwind styling for form and new pages

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :user
-  enum status: { draft: 0, published: 1, sold: 2 }
+  enum status: { draft: 0, publish: 1, sold: 2 }
 
   validates :title, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
