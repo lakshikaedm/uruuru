946d18eae80647338ac57de3c1b9d152d2badc17
Lakshika Sandaruwan <lakshikaedm@gmail.com>
2025-10-13 14:21:59 +0900

    test(devise): include ControllerHelpers for helper specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -78,6 +78,7 @@ require 'devise'
 RSpec.configure do |config|
   config.include Devise::Test::IntegrationHelpers, type: :request
   config.include Devise::Test::IntegrationHelpers, type: :system
+  config.include Devise::Test::ControllerHelpers, type: :helper
 end
 
 #Capybara
