d9b10ba16fe689c03a8f04ffd4388f1d8e084d65
Lakshika Sandaruwan <lakshikaedm@gmail.com>
2025-10-13 13:28:13 +0900

    fix(favorites): clean Stimulus controller and sync with Turbo for persistent heart state

--- app/javascript/controllers/favorite_controller.js
+++ app/javascript/controllers/favorite_controller.js
@@ -4,23 +4,16 @@ export default class extends Controller {
   static targets = ["icon"]
 
   connect() {
-    this.isFavorited = false
-    this.updateIcon()
+    this.isFavorited = this.iconTarget.classList.contains("fill-red-500")
   }
 
   toggle() {
     this.isFavorited = !this.isFavorited
     this.updateIcon()
-    this.element.setAttribute("aria-pressed", this.isFavorited.toString())
   }
 
   updateIcon() {
-    if (this.isFavorited) {
-      this.iconTarget.classList.add("fill-red-500")
-      this.iconTarget.classList.remove("fill-gray-400")
-    } else {
-      this.iconTarget.classList.add("fill-gray-400")
-      this.iconTarget.classList.remove("fill-red-500")
-    }
+    this.iconTarget.classList.toggle("fill-red-500", this.isFavorited)
+    this.iconTarget.classList.toggle("fill-gray-300", !this.isFavorited)
   }
 }
