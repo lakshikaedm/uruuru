fc3b6f0a6d25f0222fd1cb0a0d576e05e2f9cb82
Lakshika Sandaruwan <lakshikaedm@gmail.com>
2025-10-13 13:58:15 +0900

    feat(favorites): add favorites_count with counter cache and update UI to show like count

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,7 +4,7 @@ class FavoritesController < ApplicationController
 
   def create
     current_user.favorites.find_or_create_by!(product: @product)
-
+    @product.reload
     respond_to do |format|
       format.turbo_stream
       format.html { redirect_back fallback_location: product_path(@product), notice: "Added to favorites" }
@@ -13,7 +13,7 @@ class FavoritesController < ApplicationController
 
   def destroy
     current_user.favorites.find_by!(product: @product)&.destroy
-
+    @product.reload
     respond_to do |format|
       format.turbo_stream
       format.html { redirect_back fallback_location: product_path(@product), notice: "Removed from favorites" }
---
+++ db/migrate/20251013043017_add_favorites_count_to_products.rb
@@ -0,0 +1,5 @@
+class AddFavoritesCountToProducts < ActiveRecord::Migration[7.1]
+  def change
+    add_column :products, :favorites_count, :integer, default: 0, null: false
+  end
+end
