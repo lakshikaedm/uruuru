c808bedf34e020f9206d1a82552dd67fa6777dd3
Lakshika Sandaruwan <lakshikaedm@gmail.com>
2025-10-20 19:33:43 +0900

    chore: rubocop fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   enum :role, { user: 0, admin: 1 }
 
   has_many :products, dependent: :destroy
