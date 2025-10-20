67a1137e25b0d2f04e412c6170f62b2a0ae4400c
Lakshika Sandaruwan <lakshikaedm@gmail.com>
2025-10-20 19:44:58 +0900

    test(system#profile): added Profile system rspec

---
+++ spec/system/profile_system_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe "Profile", type: :system do
+  let!(:user) { create(:user, username: "User1") }
+
+  it "links from navbar to profile" do
+    sign_in user
+    visit root_path
+    click_link "User1"
+    expect(page).to have_current_path(profile_path)
+    expect(page).to have_content("User1")
+    expect(page).to have_link("❤️ Favorites", href: favorites_path)
+  end
+end
