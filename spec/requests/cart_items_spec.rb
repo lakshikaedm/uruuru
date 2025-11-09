c71c2288c8697151fdcaaa6911f2aa6abd109960
Lakshika Sandaruwan <lakshikaedm@gmail.com>
2025-11-09 21:29:37 +0900

    test(cart): verify quantity display updates before and after cart item update

--- spec/requests/cart_items_spec.rb
+++ spec/requests/cart_items_spec.rb
@@ -1,13 +1,14 @@
 require "rails_helper"
 
 RSpec.describe "CartItems", type: :request do
-  let!(:product) { create(:product, price: 1200) }
+  let!(:product) { create(:product, price: 2200) }
 
   it "adds items" do
-    post create_cart_item_path(product), params: { cart_item: { quantity: 2 } }
+    post create_cart_item_path(product), params: { cart_item: { quantity: 1 } }
     expect(response).to redirect_to(cart_path)
     follow_redirect!
     expect(response.body).to include(product.title)
+    expect(response.body).to include("1")
   end
 
   it "updates items" do
@@ -15,6 +16,7 @@ RSpec.describe "CartItems", type: :request do
     expect(response).to redirect_to(cart_path)
     follow_redirect!
     expect(response.body).to include("Subtotal")
+    expect(response.body).to include("3")
   end
 
   it "removes items" do
