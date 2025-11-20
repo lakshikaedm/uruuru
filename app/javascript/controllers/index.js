2adf12795d2c07f951f8593670415e2af8281830
Lakshika Sandaruwan <lakshikaedm@gmail.com>
2025-11-20 11:41:20 +0900

    feat(postal-code): register Stimulus controller for postal code lookup

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,3 +2,6 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
+
+import PostalCodeLookupController from "./postal_code_lookup_controller"
+application.register("postal-code-lookup", PostalCodeLookupController)
