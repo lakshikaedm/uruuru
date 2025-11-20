417b65576dfdb3b343bd08c6a64c985a28239755
Lakshika Sandaruwan <lakshikaedm@gmail.com>
2025-11-20 18:15:53 +0900

    chore(db): update schema after omniauth migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_15_040806) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_20_051909) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -168,7 +168,10 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_15_040806) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.integer "role", default: 0, null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
