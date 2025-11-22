e52b11459f78fb867b52862e12e05caddfe7e5a3
Lakshika Sandaruwan <lakshikaedm@gmail.com>
2025-11-22 21:03:52 +0900

    feat(auth): add recruiter demo login

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,6 +6,22 @@ module Users
       super
     end
 
+    def recruiter_demo
+      recruiter_email = ENV.fetch("RECRUITER_DEMO_EMAIL", "recruiter@example.com")
+      recruiter = User.find_by(email: recruiter_email)
+
+      unless recruiter
+        redirect_to new_user_session_path,
+                    alert: t("devise.sessions.recruiter_demo_missing")
+        return
+      end
+
+      sign_in(:user, recruiter)
+      redirect_to root_path,
+                  notice: t("devise.sessions.recruiter_demo_signed_in",
+                            default: "Signed in as recruiter demo user.")
+    end
+
     private
 
     def merge_cart_after_login
---
+++ spec/system/recruiter_demo_login_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe "Recruiter demo login", type: :system do
+  before do
+    create(:user, email: "recruiter@example.com", username: "Recruiter Demo")
+  end
+
+  it "logs in recruiter via navbar button" do
+    visit root_path
+
+    expect(page).to have_button(I18n.t("navbar.recruiter_demo_login"))
+
+    click_button I18n.t("navbar.recruiter_demo_login")
+
+    expect(page).to have_current_path(root_path)
+    expect(page).to have_content("Recruiter Demo")
+  end
+end
