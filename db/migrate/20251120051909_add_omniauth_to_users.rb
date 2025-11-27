6ba67f9f822befe47afc9666e422db3f9ec7443b
Lakshika Sandaruwan <lakshikaedm@gmail.com>
2025-11-27 17:17:18 +0900

    fix(users): make omniauth migration safe on fresh DB

--- db/migrate/20251120051909_add_omniauth_to_users.rb
+++ db/migrate/20251120051909_add_omniauth_to_users.rb
@@ -1,8 +1,10 @@
 class AddOmniauthToUsers < ActiveRecord::Migration[7.1]
-  change_table :users, bulk: true do |t|
-    t.string :provider
-    t.string :uid
+  def change
+    add_column :users, :provider, :string unless column_exists?(:users, :provider)
+    add_column :users, :uid, :string unless column_exists?(:users, :uid)
 
-    add_index :users, %i[provider uid], unique: true
+    unless index_exists?(:users, [:provider, :uid])
+      add_index :users, [:provider, :uid], unique: true
+    end
   end
 end
