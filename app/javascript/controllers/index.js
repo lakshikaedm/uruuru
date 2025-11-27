ea78471f589e5e1c499a9c63b6095baa3ecc38c4
Lakshika Sandaruwan <lakshikaedm@gmail.com>
2025-11-27 18:16:12 +0900

    fix(assets): use importmap-friendly imports for postal code and AI

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,7 +1,4 @@
-// Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
 
-import PostalCodeLookupController from "./postal_code_lookup_controller"
-application.register("postal-code-lookup", PostalCodeLookupController)
+eagerLoadControllersFrom("controllers", application)
